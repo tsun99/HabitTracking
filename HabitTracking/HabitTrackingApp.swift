ef8c146b57ca454624af3dacb5e8142ff25eef5d
tsun99 <tsungaila99@gmail.com>
2023-06-05 18:53:12 +0300

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Tomas Sungaila on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
