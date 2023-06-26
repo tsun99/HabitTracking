51a55cd6c30e60a6791d0c359996432db91db20f
tsun99 <tsungaila99@gmail.com>
2023-06-26 16:34:15 +0300

    Activities are now saveable along with their respective habit categories using UserDefaults

--- HabitTracking/Habits.swift
+++ HabitTracking/Habits.swift
@@ -15,12 +15,24 @@ class Habits: ObservableObject {
             }
         }
     }
-    @Published var activities = [Activity] ()
+    @Published var activities = [Activity]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(activities) {
+                UserDefaults.standard.set(encoded, forKey: "Activities")
+            }
+        }
+    }
     
     init() {
         if let savedItems = UserDefaults.standard.data(forKey: "Habits") {
             if let decodedItems = try? JSONDecoder().decode([Habit].self, from: savedItems) {
                 habits = decodedItems
+                
+                if let savedItems2 = UserDefaults.standard.data(forKey: "Activities") {
+                    if let decodedItems2 = try? JSONDecoder().decode([Activity].self, from: savedItems2) {
+                        activities = decodedItems2
+                    }
+                }
                 return
             }
         }
